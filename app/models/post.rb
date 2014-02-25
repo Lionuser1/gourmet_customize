1303a7944b54eff7fd34711a20a347f980e011f4
Lionuser <lionuser@codelion.net>
2014-02-25 04:37:15 +0000

    중간작업

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to  :user
   has_many  :comments
-  validates :category, :inclusion => { :in => ["korean","japanese","chinese","western"], :message => "한식, 일식, 중식, 양식 중 하나를 선택하셔야 합니다." }
+  validates :category, :inclusion => { :in => ["한식","일식","중식","양식"], :message => "한식, 일식, 중식, 양식 중 하나를 선택하셔야 합니다." }
   validates :title, :presence => { :message => "제목을 반드시 입력하셔야 합니다." }
 end
