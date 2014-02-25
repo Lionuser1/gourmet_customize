eb5cfb50f0a3a414aa9c1e59aac1b3ad2219eebe
Lionuser <lionuser@codelion.net>
2014-02-25 05:35:51 +0000

    게시글 작성 및 저장 기능 추가하기

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -24,6 +24,17 @@ class FoodsController < ApplicationController
   end
 
   def write_complete
+    post = Post.new
+    post.category = params[:post_category]
+    post.title = params[:post_title]
+    post.content = params[:post_content]
+    if post.save
+      flash[:alert] = "저장되었습니다."
+      redirect_to "/foods/show/#{post.id}"
+    else
+      flash[:alert] = post.errors.values.flatten.join(' ')
+      redirect_to :back
+    end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Gourmet::Application.routes.draw do
   get "/:category" => 'foods#posts_category'
   get "foods/show/:id" => 'foods#show'
   get "foods/write"
-  get "foods/write_complete"
+  post "foods/write_complete"
   get "foods/edit"
   get "foods/edit_complete"
   get "foods/delete_complete"
